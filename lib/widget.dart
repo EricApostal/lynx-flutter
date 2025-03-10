5186383308ecb3bd1c747de988dbdce49c81147f
Eric Apostal <ehapostal04@gmail.com>
2025-03-10 17:48:09 -0400

    small cleanup

--- lib/widget.dart
+++ lib/widget.dart
@@ -2,15 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class LynxView extends StatefulWidget {
-  const LynxView({Key? key}) : super(key: key);
+  const LynxView({super.key});
 
   @override
   State<LynxView> createState() => _LynxViewState();
 }
 
 class _LynxViewState extends State<LynxView> {
-  int? _textureId;
-
   @override
   void initState() {
     super.initState();
