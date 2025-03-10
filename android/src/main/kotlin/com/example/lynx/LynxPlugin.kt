ce12bfa10f24a6111aaed80dbbb69b3649e38bcf
Eric Apostal <ehapostal04@gmail.com>
2025-03-10 13:59:33 -0400

    add basic lynx init

--- android/src/main/kotlin/com/example/lynx/LynxPlugin.kt
+++ android/src/main/kotlin/com/example/lynx/LynxPlugin.kt
@@ -1,23 +1,27 @@
 package com.example.lynx
 
+import android.app.Application
+import com.lynx.tasm.LynxEnv
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import com.lynx.tasm.LynxEnv
 
 /** LynxPlugin */
-class LynxPlugin: FlutterPlugin, MethodCallHandler {
+class LynxPlugin : FlutterPlugin, MethodCallHandler {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+  private lateinit var channel: MethodChannel
 
   override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "lynx")
     channel.setMethodCallHandler(this)
+
+    val applicationContext = flutterPluginBinding.applicationContext as Application
+    LynxEnv.inst().init(applicationContext, null, null, null)
   }
 
   override fun onMethodCall(call: MethodCall, result: Result) {
